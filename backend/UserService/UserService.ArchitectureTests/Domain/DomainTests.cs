54b116d9d54d6c782a6f30e4f4ece8df0def2484
igor.p <igor.parfianets@gmail.com>
2024-11-30 13:22:40 +0300

    Implement architectural tests

---
+++ backend/UserService/UserService.ArchitectureTests/Domain/DomainTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using NetArchTest.Rules;
+using UserService.ArchitectureTests.Base;
+using UserService.Domain.Abstractions;
+
+namespace UserService.ArchitectureTests.Domain;
+
+public class DomainTests : AssemblyLayers
+{
+    [Fact]
+    public void DomainEvents_Should_BeSealed()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .BeSealed()
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainEvent_ShouldHave_DomainEventPostfix()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .HaveNameEndingWith("DomainEvent")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Entities_ShouldHave_PrivateParameterlessConstructor()
+    {
+        IEnumerable<Type> entityTypes = Types.InAssembly(DomainAssembly)
+            .That()
+            .Inherit(typeof(Entity))
+            .GetTypes();
+
+        var failingTypes = new List<Type>();
+        foreach (Type entityType in entityTypes)
+        {
+            ConstructorInfo[] constructors = entityType.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            if (!constructors.Any(c => c.IsPrivate && c.GetParameters().Length == 0))
+            {
+                failingTypes.Add(entityType);
+            }
+        }
+
+        failingTypes.Should().BeEmpty();
+    }
+}
